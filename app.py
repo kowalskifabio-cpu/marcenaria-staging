3821176780129f789366ed168cd8eb50839ac1c2
kowalskifabio-cpu <kowalski.fabio@gmail.com>
2026-04-13 14:56:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from services.pedidos_service import (
 )
 from services.auditoria_service import log_auditoria_supabase
 from services.auth_service import login
+from ui.indicadores import render_indicadores
 # =========================================================
 # CONFIGURAÇÃO INICIAL
 # =========================================================
